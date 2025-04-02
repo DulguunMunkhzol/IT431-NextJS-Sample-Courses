0509a8cbcabbb80f40570fc1185247c0be0f4044
DulguunMunkhzol <97315718+DulguunMunkhzol@users.noreply.github.com>
2025-04-01 22:12:25 -0400

    Delete utils directory

--- utils/serverFetch.ts
+++
@@ -1,5 +0,0 @@
-export default async function serverFetch(path: string, options = {}) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-  const url = new URL(path, baseUrl);
-  return fetch(url.toString(), options);
-}
